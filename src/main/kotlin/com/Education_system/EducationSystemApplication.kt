5a637e349434c4c3345dbacca52e67394af5afca
Wilson Castillo <115800617+Wilson8jsn@users.noreply.github.com>
2023-11-24 12:11:13 -0500

    Initial commit

---
+++ src/main/kotlin/com/Education_system/EducationSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.Education_system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EducationSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<EducationSystemApplication>(*args)
+}
